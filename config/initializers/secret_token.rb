ed02394321182293bfaa43c0908da76bad31697b
root <root@xhb-virtual-machine.(none)>
2013-01-02 14:01:57 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstProject::Application.config.secret_token = 'ad41f5f572522c987a95482fbbe12c1bf6fc9c2dac3739ac63154fb45e94bf196bad33b2c0fcb4d017408d7a4da4e8e5400453c882705a8dcc366a13ed8aa9c6'
